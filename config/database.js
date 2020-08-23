a3c99a7a3b29a4d6fd93a365cde6a132e54f0067
Abubakar Sadiq Kole <s.kole4real@gmail.com>
2020-08-23 05:53:05 -0700

    Initial commit

--- config/database.js
+++ config/database.js
@@ -1,18 +1,11 @@
-if(process.env.NODE_ENV === 'development'){
-    module.exports = {
-        'connection': {
-            'host': '127.0.0.1', // localde iseniz bu , local değilse ip adresini yazınız .
-            'user': 'root', // kullanıcı adı 
-            'password': '', // şifreniz 
-            'database': 'amash_pay'// database ismi .
-        },
-        'database': 'amash_pay',
-    }
-}else{
-    module.exports = {
-        
-    }
+module.exports = {
+    'connection': {
+        'host': '127.0.0.1', // localde iseniz bu , local değilse ip adresini yazınız .
+        'user': 'root', // kullanıcı adı 
+        'password': '', // şifreniz 
+        'database': 'amash_pay'// database ismi .
+    },
+    'database': 'amash_pay',
 }
-
 
 
